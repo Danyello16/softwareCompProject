2f819d274e79ad2c7fca63f4af75348aebbf3213
CsXabi <csabinagy17@gmail.com>
2022-10-05 19:00:07 +0200

    Optimize BL Entities

---
+++ src/main/java/at/fhtw/swen3/services/impl/ParcelApiController.java
@@ -0,0 +1,39 @@
+package at.fhtw.swen3.services.impl;
+
+
+import at.fhtw.swen3.services.ParcelApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.Optional;
+import javax.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-09-23T12:35:41.388911Z[Etc/UTC]")
+@Controller
+public class ParcelApiController implements ParcelApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public ParcelApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
